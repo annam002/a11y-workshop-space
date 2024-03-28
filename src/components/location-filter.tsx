9a1ef00bee323afb3fa625e1a5e2c2d61db36ee9
Florian Geierstanger <florian.geierstanger@codecentric.de>
2024-03-28 14:03:58 +0100

    5. Forms: Use radio group for location filter

--- src/components/location-filter.tsx
+++ src/components/location-filter.tsx
@@ -12,16 +12,30 @@ const LocationFilter = ({
   return (
     <>
       <div className="flex flex-col gap-4 px-4">
-        <div className="text-xl">Location</div>
-        <div className="flex flex-col">
+        <div className="text-xl" id="location">
+          Location
+        </div>
+        <div
+          className="flex flex-col"
+          role={"group"}
+          aria-labelledby="location"
+        >
           {locationFilterValues.map((filterValue) => (
-            <button
-              onClick={() => filterByLocation(filterValue)}
+            <label
               key={filterValue}
-              className={`border border-solid border-gray-200 px-6 py-2 text-left ${filterValue === currentFilter ? "bg-gray-200" : ""}`}
+              className={`cursor-pointer border border-solid border-gray-200 px-6 py-2 ${
+                filterValue === currentFilter ? "bg-gray-200" : ""
+              } focus-within:outline focus-within:outline-2 focus-within:outline-offset-4 focus-within:outline-gray-500`}
             >
+              <input
+                type="radio"
+                name="location"
+                value={filterValue}
+                onChange={() => filterByLocation(filterValue)}
+                className="appearance-none focus-visible:outline-none"
+              />
               {filterValue}
-            </button>
+            </label>
           ))}
         </div>
       </div>
