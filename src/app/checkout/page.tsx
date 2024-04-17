e142acda334ad385e51136681379119cc0a82ec2
Anna Maier <anna.maier@codecentric.de>
2024-04-17 12:50:07 +0200

    6. Structure: Add title to checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,10 @@
 import { spaceShips } from "@/api/spaceShips";
 import Checkout from "@/components/checkout/checkout";
+import { Metadata } from "playwright/test";
+
+export const metadata: Metadata = {
+  title: "Hexaspace Checkout",
+};
 
 export default async function CheckoutPage() {
   const ships = await spaceShips();
